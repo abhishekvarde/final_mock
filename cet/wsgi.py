d3384cf6570ba4471505db7cdc6cfcd9482417cf
userid007 <shindeakshay7499@gmail.com>
2021-02-26 15:51:40 +0530

    fix images not showing

--- cet/wsgi.py
+++ cet/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cet.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cet.settings")
 
 application = get_wsgi_application()
